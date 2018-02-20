3b791bddd1d39a6fef6684f71381f944b11f1ba0
Unknown <cartus@student.tugraz.at>
2018-02-20 15:33:33 +0100

    CHG: changed imports. (removed name of top package)

--- SCFInitialGuess/utilities/dataset.py
+++ SCFInitialGuess/utilities/dataset.py
@@ -12,9 +12,8 @@ import sys
 import numpy as np
 import re
 
-from utilities.constants import number_of_basis_functions as N_BASIS # todo: this is inconsitent. constantprovider class should be used.
-
-from utilities.usermessages import Messenger as msg
+from constants import number_of_basis_functions as N_BASIS
+from usermessages import Messenger as msg
 
 
 class Molecule(object):
