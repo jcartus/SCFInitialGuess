80d2d64478003e33a0bab3b8fa70c6877d23689e
Unknown <cartus@student.tugraz.at>
2018-02-20 14:11:02 +0100

    CHG: added funtionality to dummy file.

--- tests/run_all_tests.py
+++ tests/run_all_tests.py
@@ -1,1 +1,17 @@
-pass
+"""This script will run all unit test for the project.
+It was taken from 
+https://github.com/ralf-meyer/NeuralNetworks/tests/run_all_tests.py.
+
+Author:
+    - Ralf Meyer, QCIEP, TU Graz
+"""
+
+import os
+import sys
+import unittest
+
+if __name__ == '__main__':
+    testsuite = unittest.TestLoader().discover(os.path.dirname(os.path.abspath(__file__)))
+    test_runner = unittest.TextTestRunner(verbosity=1).run(testsuite)
+    if len(test_runner.failures) > 0 or len(test_runner.errors) > 0:
+        sys.exit(1)
