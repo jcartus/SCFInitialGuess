85ec673b01961389bff0b5e0874e10a1ec80c6f5
Unknown <cartus@student.tugraz.at>
2018-02-20 13:54:05 +0100

    ADD: added first setup script.

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""This script installs the package.abs
+
+Author:
+    - Johannes Cartus, QCIEP, TU Graz
+"""
+
+from distutils.core import setup
+
+name = 'SCFInitialGuess'
+
+setup(
+    name=name,
+    version='0.0',
+    description='A neural network approach to create initial guesses for scf calculations',
+    author='Johannes Cartus',
+    packages=[name, name + '.utilities']
+)
