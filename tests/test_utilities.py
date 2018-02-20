bf6ac13bc49a07926b16705ff0b6ca01b7ef80c7
Unknown <cartus@student.tugraz.at>
2018-02-20 15:49:23 +0100

    CHG: added new unit test.

---
+++ tests/test_utilities.py
@@ -0,0 +1,66 @@
+"""This module contains all tests for SCFInitialGuess.utilites part of the
+package. 
+
+Author:
+    - Johannes Cartus, QCIEP, TU Graz
+"""
+
+from os.path import normpath, join
+
+import unittest
+from helper import AbstractTest
+
+from SCFInitialGuess.utilities.dataset import XYZFileReader
+
+    
+
+
+class TestXYZFileReader(AbstractTest):
+    """This class tests XYZFileReader. """
+
+    def setUp(self):
+        self.data_folder = normpath("tests/data")
+
+        # all molecules read in will have this geometry (with variing species)
+        self.reference_positions = [
+            [0.0, 0.0,  0.0],
+            [0.0, 0.0, -2.0],
+            [0.0, 0.0,  2.0]
+        ]
+
+    def test_read_molecule_from_file(self):
+        
+        mol = XYZFileReader.read_molecule_from_file(
+            file_name=join(self.data_folder, "water.xyz"),
+            name="TestName"
+        )
+
+        # check species
+        self.assertListEqual(["O", "H", "H"], mol.species)
+
+        # check positions
+        self.assertListEqual(self.reference_positions, mol.positions)
+
+        #check Name
+        self.assertEqual("TestName", mol.full_name)
+
+    def test_read_database(self):
+        
+        mols = XYZFileReader.read_database(self.data_folder)
+        
+        # check names
+        self.assertEqual("CO2", mols[0].full_name)
+        self.assertEqual("water", mols[1].full_name)
+
+        # check species
+        self.assertListEqual(["C", "O", "O"], mols[0].species)
+        self.assertListEqual(["O", "H", "H"], mols[1].species)
+
+        # check positions
+        self.assertListEqual(self.reference_positions, mols[0].positions)
+        self.assertListEqual(self.reference_positions, mols[1].positions)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
