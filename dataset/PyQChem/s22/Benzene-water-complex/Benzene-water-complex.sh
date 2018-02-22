cd57c66aae86431dfa20851e453954c37cbb48ba
Johannes Cartus <cartus@student.tugraz.at>
2018-02-22 11:12:22 +0100

    CHG: moved pyqchem dataset into its own folder

---
+++ dataset/PyQChem/s22/Benzene-water-complex/Benzene-water-complex.sh
@@ -0,0 +1,1 @@
+qchem Benzene-water-complex.in Benzene-water-complex.out Benzene-water-complex.dir 
---
+++ dataset/PyQChem/s22/Formamide-dimer_mono1/Formamide-dimer_mono1.sh
@@ -0,0 +1,1 @@
+qchem Formamide-dimer_mono1.in Formamide-dimer_mono1.out Formamide-dimer_mono1.dir 
