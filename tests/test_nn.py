659fa7314547e8d2462ae4c4444612ef4d4c0cfd
Johannes Cartus <cartus@student.tugraz.at>
2018-03-15 23:10:01 +0100

    CHG: adding a new test.

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -12,9 +12,47 @@ from SCFInitialGuess.nn.networks import EluTrNNN
 
 class TestNetworks(unittest.TestCase):
 
+    def test_EluTrNNN_setup(self):
+
+        sess = tf.Session()
+
+        structures = [
+            [5, 5],
+            [5, 15, 5],
+            [5, 20, 15, 5]
+        ]        
+
+        for structure in structures:
+            self._test_network_setup(sess, structure, EluTrNNN(structure))
+
+    def _test_network_setup(self, sess, structure, network):
+
+        network.setup()
+
+        sess.run(tf.global_variables_initializer())
+
+        number_of_layers = len(structure) - 1
+        weights = network.weights_values(sess)
+        biases = network.biases_values(sess)
+
+        #--- check if number of layers fits ---
+        self.assertEqual(number_of_layers, len(network.weights))
+        self.assertEqual(number_of_layers, len(network.biases))
+        self.assertEqual(number_of_layers, len(weights))
+        self.assertEqual(number_of_layers, len(biases))
+        #---
+
+        for layer in range(0, number_of_layers):
+            dim_in = structure[layer]
+            dim_out = structure[layer + 1]
+
+            self.assertListEqual([dim_in, dim_out], list(weights[layer].shape))
+            self.assertEqual(dim_out, biases[layer].shape[0])
 
 
-    def test_linear_EluTrNN(self):
+
+
+    def test_linear_EluTrNNN(self):
 
         #--- setup tf and network
         sess = tf.Session()
