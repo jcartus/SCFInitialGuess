0b480589c924f2220bb5d3ca506990d863494d1b
Johannes Cartus <cartus@student.tugraz.at>
2018-03-19 23:12:24 +0100

    CHG: set print level for tests to 1

--- tests/run_all_tests.py
+++ tests/run_all_tests.py
@@ -9,8 +9,12 @@ Author:
 import os
 import sys
 import unittest
+from SCFInitialGuess.utilities.usermessages import Messenger as msg
+
 
 if __name__ == '__main__':
+    msg.print_level = 0
+
     testsuite = unittest.TestLoader().discover(os.path.dirname(os.path.abspath(__file__)))
     test_runner = unittest.TextTestRunner(verbosity=1).run(testsuite)
     if len(test_runner.failures) > 0 or len(test_runner.errors) > 0:
