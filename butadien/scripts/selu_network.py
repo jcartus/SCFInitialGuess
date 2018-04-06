0326964a7ea6d40c6a2108ab719dc8680e42f759
Unknown <cartus@student.tugraz.at>
2018-04-06 13:50:04 +0200

    CHG: added clipping to script

--- butadien/scripts/selu_network.py
+++ butadien/scripts/selu_network.py
@@ -1,5 +1,6 @@
 from SCFInitialGuess.nn.networks import SeluTrNNN
-from SCFInitialGuess.nn.training import Trainer, MSE, RegularizedMSE
+from SCFInitialGuess.nn.training import Trainer
+from SCFInitialGuess.nn.cost_functions import MSE, RegularizedMSE, AbsoluteError
 from SCFInitialGuess.utilities.dataset import Dataset
 from SCFInitialGuess.utilities.usermessages import Messenger as msg
 
@@ -44,6 +45,39 @@ class IdempotencyPenalty(MSE):
         return tf.reduce_mean(tf.norm(tf.abs(lhs - rhs), axis=(1,2)) **2 )
 
 
+class AbsoluteIdempotencyPenalty(AbsoluteError):
+
+    def __init__(self, input_transformation, coupling=1e-4):
+        
+        self.coupling = coupling
+        self.input_transformation = input_transformation
+        
+    def function(self, network, y_placeholder):
+
+        error = \
+            super(AbsoluteIdempotencyPenalty, self).function(network, y_placeholder)
+
+        penalty = self.idempotency_error(network) * self.coupling
+
+        cost = error + penalty
+
+        tf.summary.scalar("idempotency_error", penalty)
+        tf.summary.scalar("total_loss", cost)
+
+        return cost
+    
+    def idempotency_error(self, network):
+        p = tf.reshape(network.output_tensor, [-1, dim, dim])
+        s = tf.reshape(
+            self.input_transformation(network.input_tensor), 
+            [-1, dim, dim]
+        )
+        
+
+        lhs = tf.matmul(tf.matmul(p, s), p)
+        rhs = 2 * p
+        return tf.reduce_mean(tf.norm(tf.abs(lhs - rhs), axis=(1,2)) **2 )
+
 def main():   
 
     S, P = np.load("butadien/data/dataset.npy")
@@ -53,24 +87,23 @@ def main():
 
     trainer = Trainer(
         SeluTrNNN(
-            [dim**2, 400, 400, 400, 400, 400, 400, dim**2], 
+            [dim**2, 700, 700, 700, 700, dim**2], 
             log_histograms=True
         ),
-        #error_function=AbsoluteError(),
+        error_function=AbsoluteError(),
         #cost_function=RegularizedMSE(alpha=1e-7),
-        cost_function=IdempotencyPenalty(
-            dataset.inverse_input_transform,
-            coupling=1e-5
-        ),
-        optimizer=tf.train.AdamOptimizer(learning_rate=1e-3)
+        #cost_function=IdempotencyPenalty(dataset.inverse_input_transform, coupling=1e-5),
+        cost_function=AbsoluteIdempotencyPenalty(dataset.inverse_input_transform, coupling=1e-3),
+        optimizer=tf.train.AdamOptimizer(learning_rate=1e-3),
+        gradient_clipping=[1e-5,1e-2]
     )
 
     trainer.setup()
     network, sess = trainer.train(
         dataset,
-        convergence_threshold=1e-7,
+        convergence_threshold=5e-5,
         #summary_save_path="butadien/log/idem",
-        mini_batch_size=15
+        mini_batch_size=40
     )
     graph_idem = trainer.graph
 
