d990f15815c77b68baa40b6dbc25dc503cab852e
Johannes Cartus <cartus@student.tugraz.at>
2018-04-14 13:47:02 +0200

    CHG: added butadienMode to matrix_error plot function.

--- SCFInitialGuess/utilities/analysis.py
+++ SCFInitialGuess/utilities/analysis.py
@@ -4,15 +4,35 @@ Author:
     - Johannes Cartus, QCIEP, TU Graz
 """
 
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from pandas import DataFrame
 
-def matrix_error(error, xlabel="index", ylabel="index", **kwargs):
+
+def matrix_error(error, xlabel="index", ylabel="index", ButadienMode=False, **kwargs):
     
     ax = sns.heatmap(error, **kwargs)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
+
+
+    if ButadienMode:
+        C_labels = ["1s  ", "2s  ", "2px", "2py", "2pz"]
+        H_labels = ["1s  "]
+        labels = [
+            str(ci) + ". C: " + orbital \
+                for ci in range(1,5) for orbital in C_labels
+        ] + [
+            str(hi) + ". H: " + orbital \
+                for hi in range(1,7) for orbital in H_labels
+        ]
+
+
+        plt.yticks(np.arange(26), labels) 
+        plt.xticks(np.arange(26), labels, rotation='vertical') 
+
+
     return ax
     
 
@@ -39,3 +59,8 @@ def iterations_histogram(
     ax = sns.countplot(x=xlabel, y=ylabel, data=data)
     return ax
 
+if __name__ == '__main__':
+    dim = 26
+    A = np.random.rand(dim, dim)
+    matrix_error(A, orbitalTicks=True)
+    plt.show()
