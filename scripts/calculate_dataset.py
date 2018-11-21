5dc3e271e600c24cf743728fcc12651a3c31417f
Johannes Cartus <cartus@student.tugraz.at>
2018-11-21 14:47:37 +0100

    CHG: enhanced script so it can deal with QM9

--- scripts/calculate_dataset.py
+++ scripts/calculate_dataset.py
@@ -5,25 +5,35 @@ Author:
     Johannes Cartus, QCIEP, TU Graz
 """
 
-from os import listdir
+import os 
 from os.path import join
 
 from pyscf.scf import hf
 import numpy as np
 
 
-from SCFInitialGuess.utilities.dataset import Molecule, QChemResultsReader
+from SCFInitialGuess.utilities.dataset import \
+    Molecule, QChemResultsReader, XYZFileReader
 from SCFInitialGuess.utilities.usermessages import Messenger as msg
 from SCFInitialGuess.utilities.dataset import do_scf_runs
 
 
 #--- inputs ---
-dim = 46
-BASIS = "3-21++g*"
-FOLDER = "thesis/dataset/TSmall/"
+BASIS = "6-311++g**"
+FOLDER = "thesis/dataset/QM9/dsgdb9nsd.xyz"
 INDEX = None
-PREFIX = "TSmall"
+PREFIX = "QM9-300"
+FRACTION = 10000
+
+
+BASIS = "sto-3g"
+FOLDER = "thesis/dataset/EthenT/"
+INDEX = None
+PREFIX = "Test"
+FRACTION = 100
 #---
+
+
 
 
 def main():        
@@ -33,30 +43,60 @@ def main():
 
     msg.info("Fetching molecules", 2)
     molecules = QChemResultsReader.fetch_from_folder(data_folder, BASIS)
-    
+    #molecules = XYZFileReader.read_folder(data_folder, basis=BASIS)[:300] # todo calculate full list!!
 
-    if index_file is None:
-        index = np.arange(len(molecules))
-        np.random.shuffle(index)
-    else:
-        index = np.load(index_file)
-
-    molecules =[molecules[i] for i in index]
 
     msg.info("Starting SCF Calculation", 2)
-    S, P, F = do_scf_runs(molecules)
 
-    msg.info("Exporting Results", 2)
-    msg.info("Index ...", 1)
-    np.save(data_folder + "index" + PREFIX + ".npy", index)
+    if len(molecules) > FRACTION:
+        msg.info("Too many samples. Doing SCF calculations batchwise ...", 2)
 
-    msg.info("S & P ...", 1)
-    np.save(data_folder + "S" + PREFIX + ".npy", np.array(S).reshape(-1, dim**2))
-    np.save(data_folder + "P" + PREFIX + ".npy", np.array(P).reshape(-1, dim**2))
-    np.save(data_folder + "F" + PREFIX + ".npy", np.array(F).reshape(-1, dim**2))
+        number_of_batches = int(np.ceil(len(molecules) / FRACTION))
+    
+        # if dataset is too large do calculation in subsets
+        for i in range(number_of_batches):
+            
+            msg.info("Starting Subset {0}/{1}".format(
+                i + 1, 
+                number_of_batches
+            ), 2)
 
-    msg.info("Molecules ...", 1)
-    np.save(data_folder + "molecules" + PREFIX + ".npy", molecules)
+            folder = data_folder + "/SubSet_" +  str(i + 1) + "/"
+
+            try:
+                subset = molecules[i*FRACTION : (i+1) * FRACTION]
+
+                if not os.path.exists(folder):
+                    os.mkdir(folder)
+
+                S, P, F = do_scf_runs(subset)
+
+                msg.info("Exporting Results", 1)
+
+                msg.info("S & P ...", 1)
+                np.save(folder + "S" + PREFIX + ".npy", S)
+                np.save(folder + "P" + PREFIX + ".npy", P)
+                np.save(folder + "F" + PREFIX + ".npy", F)
+
+                msg.info("Molecules ...", 1)
+                np.save(folder + "molecules" + PREFIX + ".npy", subset)
+            
+            except Exception as ex:
+                msg.error("Something went wrong: " + str(ex))
+
+    else:
+
+        S, P, F = do_scf_runs(molecules)
+
+        msg.info("Exporting Results", 2)
+
+        msg.info("S & P ...", 1)
+        np.save(data_folder + "S" + PREFIX + ".npy", S)
+        np.save(data_folder + "P" + PREFIX + ".npy", P)
+        np.save(data_folder + "F" + PREFIX + ".npy", F)
+
+        msg.info("Molecules ...", 1)
+        np.save(data_folder + "molecules" + PREFIX + ".npy", molecules)
 
     msg.info("All Done. ", 2)
 
